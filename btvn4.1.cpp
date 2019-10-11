47852f81dff348ca4f6baa5425af8d628009a769
Ednguyen <nguyentrminhduc@gmail.com>
2019-10-11 23:59:41 +0700

    btvnsession4

---
+++ btvn4.1.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+	int a;
+	int b;
+	int c;
+	do {
+		printf("vui long nhap so do cac canh\n");
+		printf("so do canh thu nhat\n");
+		scanf("%d",&a);
+		printf("so do canh thu hai\n");
+		scanf("%d",&b);
+		printf("so do canh thu ba\n");
+		scanf("%d",&c);
+	}while(a+b<=c || b+c<=a || a+c<=b);
+	
+
+	
+	
+
+
+		
+		printf("day la 3 canh cua tam giac\n");
+	
+	
+	
+
+ return 0;
+}
